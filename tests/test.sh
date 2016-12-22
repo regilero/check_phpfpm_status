2233ef775128f6ea836c1d59fcf1e9da7db5b50e
regilero <regis.leroy@makina-corpus.com>
2016-12-22 17:41:02 +0100

    Fix some tests, and add some new ones

--- tests/test.sh
+++ tests/test.sh
@@ -5,29 +5,66 @@ nok=0;
 function test_last_check() {
     res=$1
     expected=$2
-    if [ "$?" -ne 0 ]; then
-        echo " ^^^^" 2>&1
-        echo " \=> FAILED TEST : last Check result [${$res}] is not the expected one [${expected}]" 2>&1
-        echo ""
-        let nok+=1
-    else
-        echo " \=> TEST is OK"
+    if [ "x${res}" == "x${expected}" ]; then
+        echo " \=> TEST is OK ("${res}" == "${expected}")"
         echo ""
         let ok+=1
+    else
+        echo " ^^^^" 2>&1
+        echo " \=> FAILED TEST : last Check result ("${res}") is not the expected one ("${expected}")" 2>&1
+        echo ""
+        let nok+=1
     fi
 }
 
-echo " * Checking phpfpm is fastcgi mode --------------------------------------"
+echo " ------------------------------------------------------------------------"
+echo "                FASTCGI MODE"
+echo " ------------------------------------------------------------------------"
+echo
+echo " * Checking phpfpm in fastcgi mode --------------------------------------"
+./check_phpfpm_status.pl -H 127.0.0.1 -p 9001 -u /check-status --fastcgi
+test_last_check ${?} 0
+
+echo " * Request the phpinfo page, -will fail the check -----------------------"
+./check_phpfpm_status.pl -H 127.0.0.1 -p 9001 -u /usr/src/app/index.php --fastcgi
+test_last_check ${?} 2
+
+echo " * Checking phpfpm in fastcgi mode --------------------------------------"
 ./check_phpfpm_status.pl -H 127.0.0.1 -p 9001 -u /check-status --fastcgi
 test_last_check ${?} 0
 
 
-echo " * Request the phpinfo page, -will fail the check -----------------------"
-./check_phpfpm_status.pl -H 127.0.0.1 -p 9001 -u /usr/src/app/index.php --fastcgi
-test_last_check ${?} 1
+echo " ------------------------------------------------------------------------"
+echo "                HTTP MODE"
+echo " ------------------------------------------------------------------------"
+echo
+echo " * Checking phpfpm in http mode : no server name, should fail ----------"
+./check_phpfpm_status.pl -t 15 -H 127.0.0.1 -p 8801 -s -u /check-status
+test_last_check ${?} 2
 
-echo " * Checking phpfpm is fastcgi mode --------------------------------------"
-./check_phpfpm_status.pl -H 127.0.0.1 -p 9001 -u /check-status --fastcgi
+echo " * Checking phpfpm in http mode : bad server name, should fail ----------"
+./check_phpfpm_status.pl -t 15 -H 127.0.0.1 -p 8801 -s doesnotexists.example.com -u /check-status
+test_last_check ${?} 2
+
+echo " * Checking phpfpm in http mode : right server name ---------------------"
+./check_phpfpm_status.pl -t 15 -H 127.0.0.1 -p 8801 -s phpfpm.example.com -u /check-status
+test_last_check ${?} 0
+
+echo " * Checking phpfpm in http mode : right server name, bad url-------------"
+./check_phpfpm_status.pl -t 15 -H 127.0.0.1 -p 8801 -s phpfpm.example.com -u /checkstatus
+test_last_check ${?} 2
+
+echo " ------------------------------------------------------------------------"
+echo "                HTTPS MODE"
+echo " ------------------------------------------------------------------------"
+echo
+
+echo " * Checking phpfpm in https mode, https on http port failre--------------"
+./check_phpfpm_status.pl -t 15 -H 127.0.0.1 -p 8801 -s phpfpm.example.com -u /check-status --ssl
+test_last_check ${?} 2
+
+echo " * Checking phpfpm in https mode, https on http port failre--------------"
+./check_phpfpm_status.pl -t 15 -H 127.0.0.1 -p 8443 -s phpfpm.example.com -u /check-status --ssl
 test_last_check ${?} 0
 
 echo
